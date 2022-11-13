ae47162a8857ec5e76f1a18c28bf2d7eb04db232
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-11-14 00:17:51 +0700

    URL Model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Url(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    oldUrl = models.TextField(null=False, blank=False),
+    newUrl = models.TextField(),
+    create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.newUrl
+
+    class Meta:
+        ordering = ['create']
