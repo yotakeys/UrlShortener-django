0ae73710ec1a416ebbf78ca967b99dda712b1c77
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-11-14 22:16:58 +0700

    User Registration and Login

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import Login, Register, UrlList
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', name='home', view=home),
+    path('', name='url', view=UrlList.as_view()),
+    path('login', name='login', view=Login.as_view()),
+    path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
+    path('register', name='register', view=Register.as_view()),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect
 
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import FormView
+from django.views.generic.list import ListView
+
+from django.contrib.auth import login
+from .models import Url
 # Create your views here.
 
 
-def home(request):
-    return HttpResponse("Hellow world")
+class Login(LoginView):
+    template_name = "login.html"
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('url')
+
+
+class Register(FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('url')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(Register, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(Register, self).get(*args, **kwargs)
+
+
+class UrlList(LoginRequiredMixin, ListView):
+    model = Url
+    template_name = 'url_list.html'
